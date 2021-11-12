d109a0395886b12c9dbeddba45effa3398102b3a
unknown <limah@coventry.ac.uk>
2021-11-12 17:26:43 +0000

    rats rats rats rats rats

---
+++ SpookyGame/Assets/Particles/Rats/RatSueak.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatSueak : MonoBehaviour
+{
+    public Transform[] SquealPos;
+    public GameObject[] Squeak;
+    ParticleSystem particles;
+    float spawntime;
+    int index=0;
+
+
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+        spawntime = particles.main.duration;
+        StartCoroutine(SquealNumerator());
+    }
+    IEnumerator SquealNumerator()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawntime + 0.00000001f);
+            if (particles.particleCount > 0)
+            {
+                StartCoroutine(SqueakSpot());
+
+
+            }
+
+        }
+        
+
+    }
+
+    IEnumerator SqueakSpot()
+    {
+
+        for (int i = 0; i < 3; i++)
+        {
+           
+            yield return new WaitForSeconds(1.6f);
+           
+            if (Vector3.Distance(GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Rigidbody>().transform.position, SquealPos[index].position) < 5)
+            {
+              
+                Instantiate(Squeak[Random.Range(0, Squeak.Length)], SquealPos[index].position, Quaternion.identity);
+
+
+            }
+            index++;
+
+
+        }
+        index = 0;
+    }
+
+ 
+
+  
+}
