e8682cd9fde0a83cc7150f1fc333838b8f77a8c3
unknown <limah@coventry.ac.uk>
2021-11-15 21:47:50 +0000

    hell

---
+++ SpookyGame/Assets/Props/Clock/Scripts/Clock.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+   
+
+    float secondsWorth;
+    [SerializeField] Transform secondPointer;
+    [SerializeField] Transform hourPointer;
+
+   public int seconds;
+   public int minutes;
+    float time=0;
+     void Start()
+    {
+
+        //10 mins
+        secondsWorth = 0.0303030303f;
+       
+
+     
+    }
+  
+
+    void Update()
+    {
+       
+       
+            if(time >= secondsWorth)
+           {
+           
+            time -= secondsWorth;
+            seconds++;
+            if (seconds >= 60)
+            {
+                seconds = 0;
+
+                minutes++;
+
+                secondPointer.transform.Rotate(0, -6f, 0);
+
+                hourPointer.transform.Rotate(0, -0.5f, 0);
+            }
+
+           }
+
+
+
+        time += Time.deltaTime;
+        
+    }
+       
+
+
+}
